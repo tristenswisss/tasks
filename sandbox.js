b2432aee8afe5e926bc804cfcf7f8d49756cfc6f
craiguncommon <116950355+tristenswisss@users.noreply.github.com>
2023-05-09 08:36:20 +0200

    added index and js

---
+++ sandbox.js
@@ -0,0 +1,27 @@
+const pi = 3.14;
+let my_name = 'craig';
+let age = 23;
+
+let greeting = 'Hello john!';
+
+const calc =  (a,b)=> {
+  return a+b
+
+}
+
+console.log(calc(10, 12));
+
+console.log(`my name is  ${my_name} and im ${age} years old`);
+console.log(greeting)
+
+
+//Terminal commands
+
+/*Cd    entering a directory
+Cd      entering a directory
+Pwd.    Previewing current directory
+Ls.     Listing the current directory
+mkdir   making a directory
+Mv      moving a file
+Q       quiting or terminating a command
+*/
